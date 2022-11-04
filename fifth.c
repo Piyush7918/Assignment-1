a285b2bb6a65c8fc1659401eb78bd50654f1b769
Piyush7918 <105233195+Piyush7918@users.noreply.github.com>
2022-11-04 13:57:25 +0530

    Add files via upload

---
+++ fifth.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+    {
+       int A;
+       A=printf("Piyush");
+
+       printf("\nLength of the string = %d\n",A);
+return 0;
+}
---
+++ fourth.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+   float r,a ;
+   printf("value of r=");
+   scanf("%f",&r);
+   a = (3.14)*r*r;
+   printf("The area of the circle is %f",a);
+   return 0;
+}
